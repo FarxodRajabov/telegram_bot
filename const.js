ac93d5ee8901a7d4e460b35e3efa2e3ae6410dce
Farxod <96386942+FarxodRajabov@users.noreply.github.com>
2022-07-19 15:26:12 +0500

    working

--- const.js
+++ const.js
@@ -1,6 +1,7 @@
 const commands = `
 /start - Перезапустить бота
 /help - Помощь
+/course - ???
 `
 
 module.exports.commands = commands
--- index.js
+++ index.js
@@ -1,10 +1,18 @@
-const { Telegraf } = require('telegraf')
+const { Telegraf, Markup } = require('telegraf')
 require('dotenv').config()
 const commConst = require('./const')
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
 bot.start((ctx) => ctx.reply(`Hello ${ctx.message.from.first_name}`))
 bot.help((ctx) => ctx.reply(commConst.commands))
+bot.command('course', ctx => {
+    ctx.replyWithHTML('<b>Courses</b>', Markup.inlineKeyboard(
+        [
+            [Markup.button.callback('Edits', 'btn_1'),Markup.button.callback('View', 'btn_2')],
+            [Markup.button.callback('Edits', 'btn_1'),Markup.button.callback('View', 'btn_2')]
+        ]
+    ))
+})
 
 bot.launch()
 
